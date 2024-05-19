78785945de87c631ba7b09c0d79070b4075273a2
Martin <78699859+moretime-dev@users.noreply.github.com>
2024-05-19 14:46:22 +0200

    Feat/#1 add beautiful dnd package (#2)
    * #1 Add beautiful-dnd Package
    
    - installed the package
    - took some basic code from this article https://www.freecodecamp.org/news/how-to-add-drag-and-drop-in-react-with-react-beautiful-dnd/
    - it gets a "Unable to find draggable with id: X" error and the dragging does not work --> removing React Strict Mode solves the issue, but further investigatino may be needed as this workaround doesn't feel right
    
    * #1 Add beautiful-dnd Package
    
    - possible solution for the StrictMode cant find draggable id problem is using this library instead: https://github.com/hello-pangea/dnd

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import { Button } from "./components/ui/button";
+import App from "./App";
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <h1 className="text-3-xl font-bold underline">HELLO World</h1>
-    <Button>Click me</Button>
-  </React.StrictMode>
-);
+ReactDOM.createRoot(document.getElementById("root")!).render(<App />);
