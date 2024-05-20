34ba6b53beba4026ca54ff351f16a1364d8ef340
moretime-dev <moretimeproductions@googlemail.com>
2024-05-20 20:28:47 +0200

    Add shadecn Popover shell for creating new issue #5
    - added shadcn Select Field for choosing the type of a ticket
    - handling was a bit tricky,, but works now, some rtfm of zod seems to be necessary

--- src/TicketForm.tsx
+++ src/TicketForm.tsx
@@ -14,23 +14,39 @@ import {
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
 import { Textarea } from "@/components/ui/textarea";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import { useState } from "react";
 
 const formSchema = z.object({
   title: z.string().min(2, {
-    message: "title must be at least 2 characters.",
+    message: "Title must be at least 2 characters.",
   }),
   description: z.string().min(2, {
-    message: "description must be at least 2 characters.",
+    message: "Description must be at least 2 characters.",
   }),
+  type: z.string({ message: "Please choose a ticket type" }),
 });
 
 export default function TicketForm() {
+  const [ticketType, setTicketType] = useState<string>("");
+
   // 1. Define your form.
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
     defaultValues: {
       title: "",
       description: "",
+    },
+    values: {
+      title: "",
+      description: "",
+      type: ticketType,
     },
   });
 
@@ -42,10 +58,39 @@ export default function TicketForm() {
     console.log(values);
   }
 
+  function handleSelectType(value: string) {
+    setTicketType(value);
+  }
+
   return (
     <div className="w-96">
       <Form {...form}>
         <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+          <FormField
+            control={form.control}
+            name="type"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Type of Ticket</FormLabel>
+                <Select
+                  {...field}
+                  onValueChange={(value: string) => handleSelectType(value)}
+                >
+                  <SelectTrigger className="w-[180px]">
+                    <SelectValue placeholder="Select Type" />
+                  </SelectTrigger>
+                  <SelectContent>
+                    <SelectItem value="Story">Story</SelectItem>
+                    <SelectItem value="Hotfix">Hotfix</SelectItem>
+                    <SelectItem value="Test">Test</SelectItem>
+                    <SelectItem value="Documentation">Documentation</SelectItem>
+                    <SelectItem value="Styling">Styling</SelectItem>
+                  </SelectContent>
+                </Select>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
           <FormField
             control={form.control}
             name="title"
@@ -55,7 +100,6 @@ export default function TicketForm() {
                 <FormControl>
                   <Input placeholder="title" {...field} />
                 </FormControl>
-                <FormDescription></FormDescription>
                 <FormMessage />
               </FormItem>
             )}
@@ -69,7 +113,6 @@ export default function TicketForm() {
                 <FormControl>
                   <Textarea placeholder="description" {...field} />
                 </FormControl>
-                <FormDescription></FormDescription>
                 <FormMessage />
               </FormItem>
             )}
